15e50ebff844a58f07f26d52394126d2e10c76a4
FranPisarri <franciscopisarri@gmail.com>
2024-01-25 14:30:35 -0300

    0.1

--- Assets/Scripts/PJ/Inventory.cs
+++ Assets/Scripts/PJ/Inventory.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class Inventory : MonoBehaviour
 {
     [SerializeField] private bool jokerNose = false;
-    [SerializeField] private bool item2 = false;
+    [SerializeField] private bool[] cards;
     [SerializeField] private bool item3 = false;
     [SerializeField] private bool item4 = false;
     [SerializeField] private bool item5 = false;
+    
 }

44b06204db4632c19332fdaf093411813eb6807a
GastonBernardini <124274459+GastonBernardini@users.noreply.github.com>
2024-01-26 21:20:01 -0300

    Cod MovementPJ

--- Assets/Scripts/PJ/Movement_Player.cs
+++ Assets/Scripts/PJ/Movement_Player.cs
@@ -7,9 +7,12 @@ public class Movement_Player : MonoBehaviour
 
     public float velocity;
     private Vector2 direction = Vector2.zero;
+    private Rigidbody2D rb;
+
     void Start()
     {
         transform.position = Inventory.Instance.spawnCoordinates;
+        rb = GetComponent<Rigidbody2D>();
     }
 
 
@@ -17,42 +20,24 @@ public class Movement_Player : MonoBehaviour
     {
         float arriba = Input.GetAxis("Vertical");
         float derecha = Input.GetAxis("Horizontal");
-        float deltaTime = Time.deltaTime;
+        
 
-        float x = 0;
-        float y = 0;
-        if (arriba > 0)
-        {
-            y = 1;
-        }
-        if (arriba < 0)
-        {
-            y = -1;
-        }
+        var newDirection = new Vector2(derecha, arriba).normalized;
         if (derecha > 0)
         {
-            x = 1;
             transform.localScale = Vector3.one;
         }
         if (derecha < 0)
         {
-            x = -1;
             transform.localScale = new Vector3(-1, 1, 1);
         }
-        if ( x != 0 && y != 0)
-        {
-            x *= 0.7f;
-            y *= 0.7f;
-        }
-
-        direction = new Vector2(velocity * x * Time.deltaTime, velocity * y * Time.deltaTime);
-
+        direction = newDirection * velocity;
 
 
     }
 
     private void FixedUpdate()
     {
-        this.GetComponent<Rigidbody2D>().velocity = direction*1000;
+        rg.velocity = direction;
     }
 }

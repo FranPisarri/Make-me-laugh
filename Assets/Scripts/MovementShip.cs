04a78274319c2e4b7222892c63f0a9ed390337f3
FranPisarri <franciscopisarri@gmail.com>
2024-01-27 15:55:17 -0300

    popUP desactivado

--- Assets/Scripts/MovementShip.cs
+++ Assets/Scripts/MovementShip.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class MovementShip : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Vector2 movePoint1;
+    public Vector2 movePoint2;
+    private Vector2 movePoint;
+    public float speed;
+
     void Start()
     {
-        
+        movePoint = movePoint2;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.x <= movePoint1.x)
+        {
+            movePoint = movePoint2;
+        }
+        if (transform.position.x >= movePoint2.x)
+        {
+            movePoint = movePoint1;
+        }
+
+        transform.Translate(new Vector2(movePoint.x - transform.position.x, movePoint.y - transform.position.y).normalized*speed*Time.deltaTime);
         
     }
 }

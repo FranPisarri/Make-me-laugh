a5f2aea9c49338f40a4543dd5c274fd926ada027
FranPisarri <franciscopisarri@gmail.com>
2024-01-27 13:49:51 -0300

    Dialogo duck vs Cat sin pelea

---
+++ Assets/Scripts/TarotGuy.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+
+public class TarotGuy : MonoBehaviour
+{
+
+    public string[] dialogos1;
+    public string[] dialogos2;
+    public string[] dialogos3;
+    private int dialogIndex;
+    private bool dialogFinish = false;
+    public TextMeshProUGUI textMesh;
+    public GameObject pj;
+    private int cardQuantity;
+
+
+    [SerializeField] private TextWriter _textWriter;
+
+
+    void Start()
+    {
+        dialogIndex = 0;
+        _textWriter.AddWriter(textMesh, dialogos1[dialogIndex], 0.1f);
+        cardQuantity = 0;
+
+    }
+
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.Space) && !_textWriter.IsWriting)
+        {
+            for (int i = 0; i < Inventory.Instance.cards.Length; i++)
+            {
+                if (Inventory.Instance.cards[i])
+                {
+                    cardQuantity++;
+                }
+            }
+            if (cardQuantity < Inventory.Instance.cards.Length)
+            {
+
+                dialogIndex++;
+                if (!dialogFinish)
+                {
+                    if (dialogIndex < dialogos1.Length) { UpdateDialog(dialogIndex, dialogos1); }
+                    else
+                    {
+                        pj.GetComponent<Movement_Player>().enabled = true;
+                        gameObject.SetActive(false);
+                        dialogIndex = 0;
+                        dialogFinish = true;
+                    }
+                }
+                else
+                {
+                    if (dialogIndex < dialogos2.Length)
+                    {
+                        string newDialogue;
+                        newDialogue = dialogos2[dialogIndex];
+                        if (dialogIndex == 2)
+                        {
+                            newDialogue = dialogos2[dialogIndex] + cardQuantity.ToString();
+
+                        }
+                        UpdateDialog(newDialogue);
+                    }
+                    else
+                    {
+                        pj.GetComponent<Movement_Player>().enabled = true;
+                        gameObject.SetActive(false);
+                        dialogIndex = 0;
+                    }
+                }
+                cardQuantity = 0;
+            }
+            else
+            {
+                if (dialogIndex < dialogos3.Length) { UpdateDialog(dialogIndex, dialogos3); }
+                else
+                {
+                    pj.GetComponent<Movement_Player>().enabled = true;
+                    gameObject.SetActive(false);
+                }
+                dialogIndex++;
+            }
+
+        }
+    }
+
+    private void UpdateDialog(int x, string[] dialogos)
+    {
+
+        _textWriter.AddWriter(textMesh, dialogos[x], 0.1f);
+    }
+
+    private void UpdateDialog(string newDialogue)
+    {
+        _textWriter.AddWriter(textMesh, newDialogue, 0.1f);
+    }
+}

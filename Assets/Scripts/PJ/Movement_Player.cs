cd24cafcab3ff507477071942e3356443bc474f0
Francou2000 <francourbini@gmail.com>
2024-01-28 15:06:35 -0500

    Animacion de caminar otto

--- Assets/Scripts/PJ/Movement_Player.cs
+++ Assets/Scripts/PJ/Movement_Player.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Movement_Player : MonoBehaviour
 {
+
+    public Animator anim;
 
     public float velocity;
     private Vector2 direction = Vector2.zero;
@@ -13,6 +16,7 @@ public class Movement_Player : MonoBehaviour
     {
         transform.position = Inventory.Instance.spawnCoordinates;
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
 
@@ -23,14 +27,34 @@ public class Movement_Player : MonoBehaviour
         
 
         var newDirection = new Vector2(derecha, arriba).normalized;
+
         if (derecha > 0)
         {
             transform.localScale = Vector3.one;
+            anim.SetTrigger("Side");
+            anim.SetBool("Idle", false);
         }
         if (derecha < 0)
         {
             transform.localScale = new Vector3(-1, 1, 1);
+            anim.SetTrigger("Side");
+            anim.SetBool("Idle", false);
         }
+        if (arriba > 0)
+        {
+            anim.SetTrigger("Up");
+            anim.SetBool("Idle", false);
+        }
+        if (arriba < 0)
+        {
+            anim.SetTrigger("Down");
+            anim.SetBool("Idle", false);
+        }
+        if (derecha == 0 && arriba == 0)
+        {
+            anim.SetBool("Idle", true);
+        }
+
         direction = newDirection * velocity;
 
 

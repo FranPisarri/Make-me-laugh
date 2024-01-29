e444831b9a1fa7bde96574c14d24d6d64ad0a467
SiragusWolf <76983197+SiragusWolf@users.noreply.github.com>
2024-01-29 13:53:58 -0300

    Fixed diagonal isometric movement
    Made it so the cat moves diagonally according to isometric view instead of 45° angle, making it so movement matches walk animation

--- Assets/Scripts/PJ/Movement_Player.cs
+++ Assets/Scripts/PJ/Movement_Player.cs
@@ -25,46 +25,61 @@ public class Movement_Player : MonoBehaviour
     {
         float arriba = Input.GetAxis("Vertical");
         float derecha = Input.GetAxis("Horizontal");
-        
 
-        var newDirection = new Vector2(derecha, arriba).normalized;
+
+        Vector2 newDirection;
+
+        newDirection = new Vector2(0, 0);
 
         if (derecha > 0)
         {
             transform.localScale = Vector3.one;
             anim.SetInteger("Walk", 1);
+            newDirection = new Vector2(derecha, arriba).normalized;
         }
         if (derecha < 0)
         {
             transform.localScale = new Vector3(-1, 1, 1);
             anim.SetInteger("Walk", 1);
+            newDirection = new Vector2(derecha, arriba).normalized;
         }
         if (arriba > 0)
         {
             anim.SetInteger("Walk", 2);
+            newDirection = new Vector2(derecha, arriba).normalized;
         }
         if (arriba < 0)
         {
             anim.SetInteger("Walk", 3);
+            newDirection = new Vector2(derecha, arriba).normalized;
         }
-        if (derecha > 0 && arriba > 0)
+
+        if (derecha > 0 && arriba > 0) //arriba derecha
         {
             anim.SetInteger("Walk", 5);
+            newDirection = new Vector2(0.866f, 0.45f).normalized;
         }
-        if (derecha > 0 && arriba < 0) 
+
+        if (derecha > 0 && arriba < 0)  //abajo derecha
         {
             anim.SetInteger("Walk", 6);
+            newDirection = new Vector2(0.866f, -0.45f).normalized;
         }
-        if (derecha < 0 && arriba > 0)
+
+        if (derecha < 0 && arriba > 0) //arriba izquierda
         {
             transform.localScale = new Vector3(-1, 1, 1);
             anim.SetInteger("Walk", 5);
+            newDirection = new Vector2(-0.866f, 0.45f).normalized;
         }
-        if (derecha < 0 && arriba < 0)
+
+        if (derecha < 0 && arriba < 0) //abajo izquierda
         {
             transform.localScale = new Vector3(-1, 1, 1);
             anim.SetInteger("Walk", 6);
+            newDirection = new Vector2(-0.866f, -0.45f).normalized;
         }
+
         if (derecha == 0 && arriba == 0)
         {
             anim.SetInteger("Walk", 0);
